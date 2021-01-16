fa3ddc28b959d5b659eb74da0ea925e3ded1557d
crester <druggygangsta@gmail.com>
2021-01-17 02:34:11 +0700

    added autowear delay

--- gamemode/core/bodyarmor/sv_autowear.lua
+++ gamemode/core/bodyarmor/sv_autowear.lua
@@ -1,8 +1,10 @@
 local ids = {"vest", "bicep", "forearm", "calf", "thigh"}
 hook.Add("PlayerInitialSpawn", "Fray Body Armor", function(pl)
-	for _, id in pairs(ids) do
-		if pl:HasInventoryItem("fray_ba_" .. id) then 
-			pl:SetBodyArmor(id, true)
+	timer.Simple(0.5, function()
+		for _, id in pairs(ids) do
+			if pl:HasInventoryItem("fray_ba_" .. id) then 
+				pl:SetBodyArmor(id, true)
+			end
 		end
-	end
+	end)
 end)
