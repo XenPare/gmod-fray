67ecc45d2b2c1d28d07f8a2b980f44d6ed4131ba
crester <druggygangsta@gmail.com>
2021-01-16 20:40:33 +0700

    created loot base, applied it to armor parts

--- entities/entities/fray_ba_calf.lua
+++ entities/entities/fray_ba_calf.lua
@@ -1,29 +1,11 @@
 AddCSLuaFile()
 
 ENT.Type = "anim"
-ENT.Base = "base_gmodentity"
+ENT.Base = "fray_loot"
 
 ENT.PrintName = "Calf"
 ENT.Category = "Fray Body Armor"
 ENT.Author = "crester"
 
 ENT.Spawnable = true
-
-if SERVER then
-	function ENT:Initialize()
-		self:SetModel("models/snowzgmod/payday2/armour/armourlcalf.mdl")
-		self:PhysicsInit(SOLID_VPHYSICS)
-		self:SetMoveType(MOVETYPE_VPHYSICS)
-		self:SetSolid(SOLID_VPHYSICS)
-		self:SetUseType(SIMPLE_USE)
-	end
-
-	function ENT:Use(pl)
-		pl:AddInventoryItem(self)
-		self:Remove()
-	end
-else
-	function ENT:Draw()
-		self:DrawModel()
-	end
-end
+ENT.Model = "models/snowzgmod/payday2/armour/armourlcalf.mdl"
---
+++ entities/entities/fray_loot.lua
@@ -0,0 +1,30 @@
+AddCSLuaFile()
+
+ENT.Type = "anim"
+ENT.Base = "base_gmodentity"
+
+ENT.PrintName = "Loot Base"
+ENT.Category = "Fray"
+ENT.Author = "crester"
+
+ENT.Spawnable = false
+ENT.Model = "models/squad/sf_plates/sf_plate1x1.mdl"
+
+if SERVER then
+	function ENT:Initialize()
+		self:SetModel(self.Model)
+		self:PhysicsInit(SOLID_VPHYSICS)
+		self:SetMoveType(MOVETYPE_VPHYSICS)
+		self:SetSolid(SOLID_VPHYSICS)
+		self:SetUseType(SIMPLE_USE)
+	end
+
+	function ENT:Use(pl)
+		pl:AddInventoryItem(self)
+		self:Remove()
+	end
+else
+	function ENT:Draw()
+		self:DrawModel()
+	end
+end
