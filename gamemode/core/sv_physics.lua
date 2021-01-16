93d419fe5740d85dfce8eee5a750c50654927469
crester <druggygangsta@gmail.com>
2021-01-16 16:56:33 +0700

    disable prop_physics dmg

--- gamemode/core/sv_physics.lua
+++ gamemode/core/sv_physics.lua
@@ -1,10 +1,16 @@
 hook.Add("InitPostEntity", "Fray", function()
-    for _, ent in pairs(ents.GetAll()) do
-        if ent:GetClass() == "prop_physics" then
-            local phys = ent:GetPhysicsObject()
-            if IsValid(phys) then
-                phys:EnableMotion(false)
-            end
-        end
-    end
+	for _, ent in pairs(ents.GetAll()) do
+		if ent:GetClass() == "prop_physics" then
+			local phys = ent:GetPhysicsObject()
+			if IsValid(phys) then
+				phys:EnableMotion(false)
+			end
+		end
+	end
+end)
+
+hook.Add("EntityTakeDamage", "Fray", function(target, dmg)
+	if target:GetClass() == "prop_physics" then
+		dmg:SetDamage(0)
+	end
 end)
