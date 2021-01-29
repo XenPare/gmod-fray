a6e255b3101c6498959d2768fe58a9c02ff97db2
crester <druggygangsta@gmail.com>
2021-01-29 20:19:03 +0700

    added loot label on ent hover

---
+++ gamemode/core/cl_label.lua
@@ -0,0 +1,28 @@
+hook.Add("PostGamemodeLoaded", "Fray Loot Label", function()
+	for _, att in pairs(Fray.Config.Loot) do
+		local ENT = scripted_ents.GetStored(att).t
+		function ENT:Draw()
+			self:DrawModel()
+		end
+	end
+	for att in pairs(Fray.Config.Attachments) do
+		local ENT = scripted_ents.GetStored(att).t
+		function ENT:Draw()
+			self:DrawModel()
+		end
+	end
+end)
+
+local pl, tr
+local x, y = ScrW() / 2, ScrH() / 2 + 128
+hook.Add("HUDPaint", "Fray Loot Label", function()
+	pl = LocalPlayer()
+	ent = pl:GetEyeTrace().Entity
+	if not IsValid(ent) then
+		return
+	end
+	if Fray.InventoryList[ent:GetClass()] then
+		draw.SimpleText(Fray.GetPhrase(Fray.InventoryList[ent:GetClass()].label), "xpgui_huge", x, y + 1, ColorAlpha(color_black, 240), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
+		draw.SimpleText(Fray.GetPhrase(Fray.InventoryList[ent:GetClass()].label), "xpgui_huge", x, y, color_white, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
+	end
+end)
