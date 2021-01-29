4d5491f637501eaa7d6fcfc65cb6195e5c8c6f7e
crester <druggygangsta@gmail.com>
2021-01-29 20:30:50 +0700

    halos correction

--- gamemode/core/cl_halos.lua
+++ gamemode/core/cl_halos.lua
@@ -3,17 +3,19 @@ net.Receive("Fray Broadcast Loot", function()
 	active = net.ReadTable()
 end)
 
-local usable = {
-	"fray_deliver",
-	"fray_weapon"
-}
+local dropped = {}
+hook.Add("OnEntityCreated", "Fray Halos", function(ent)
+	if not Fray.InventoryList[ent:GetClass()] then
+		return
+	end
+	table.insert(dropped, ent)
+end)
 
 local inv = nil
 local m_rg = Color(153, 66, 69)
 hook.Add("PreDrawHalos", "Fray Halos", function()
 	halo.Add(active, color_white, 1, 1, 2)
+	halo.Add(dropped, color_white, 1, 1, 2)
 	halo.Add(ents.FindByClass("prop_ragdoll") , m_rg, 1, 1, 2)
-	for _, class in pairs(usable) do
-		halo.Add(ents.FindByClass(class), color_white, 1, 1, 2)
-	end
+	halo.Add(ents.FindByClass("fray_deliver") , color_white, 1, 1, 2)
 end)
