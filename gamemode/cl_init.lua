7ba74225fc8fe76d749da162378946135483ad4c
crester <druggygangsta@gmail.com>
2021-01-31 06:01:06 +0700

    removed target id, gonna replace with 3d2d soon

--- gamemode/cl_init.lua
+++ gamemode/cl_init.lua
@@ -3,6 +3,10 @@ include("lang.lua")
 
 local CFG = Fray.Config
 
+function GM:HUDDrawTargetID()
+	return
+end
+
 local tohide = CFG.HideHUD
 hook.Add("HUDShouldDraw", "Fray", function(name)
 	if tohide[name] then 
