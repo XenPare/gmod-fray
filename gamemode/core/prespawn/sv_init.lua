04284e13ab757742ac48219b20cda9bbd60d83e2
crester <druggygangsta@gmail.com>
2021-05-18 01:28:18 +0700

    exploitable fix

--- gamemode/core/prespawn/sv_init.lua
+++ gamemode/core/prespawn/sv_init.lua
@@ -3,6 +3,10 @@ local time = Fray.Config.BabygodTime
 util.AddNetworkString("Fray Spawn")
 
 net.Receive("Fray Spawn", function(_, pl)
+	if pl.Spawned then
+		return
+	end
+
 	pl.Spawned = true
 	pl:Spawn()
 	pl:SetNWBool("Babygod", true)
