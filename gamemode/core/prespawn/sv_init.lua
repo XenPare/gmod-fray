4bc8bb807f24c3e487fc072c3af75d4a283cee81
crester <druggygangsta@gmail.com>
2021-05-19 11:42:59 +0700

    fixed inventory weight calculating after prespawn

--- gamemode/core/prespawn/sv_init.lua
+++ gamemode/core/prespawn/sv_init.lua
@@ -1,5 +1,8 @@
 local time = Fray.Config.BabygodTime
 
+local def_run = Fray.Config.RunSpeed
+local def_jump = Fray.Config.JumpPower
+
 util.AddNetworkString("Fray Spawn")
 
 net.Receive("Fray Spawn", function(_, pl)
@@ -10,6 +13,9 @@ net.Receive("Fray Spawn", function(_, pl)
 	pl.Spawned = true
 	pl:Spawn()
 	pl:SetNWBool("Babygod", true)
+
+	pl:SetRunSpeed(def_run - pl:CalculateInventoryWeight())
+	pl:SetJumpPower(def_jump - (math.Round(pl:CalculateInventoryWeight() / 2)))
 
 	hook.Call("PostPlayerSpawn", GAMEMODE, pl)
 
