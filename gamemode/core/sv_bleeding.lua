41637c9586104fbceff238a352de16d4a01ba2ed
crester <druggygangsta@gmail.com>
2021-05-19 12:53:42 +0700

    added chance to stop bleeding (~8% by current config)

--- gamemode/core/sv_bleeding.lua
+++ gamemode/core/sv_bleeding.lua
@@ -1,12 +1,17 @@
+local chance = Fray.Config.BleedingChance
 timer.Create("Fray Bleeding", 3, 0, function()
 	for _, pl in pairs(player.GetAll()) do
-		if pl:GetNWBool("Fray Bleeding") then
+		if pl:GetNWBool("Fray Bleeding") and pl:Alive() then
 			pl:TakeDamage(math.random(1, 3), pl)
+			if pl:Alive() then
+				if math.random(1, 100) <= math.floor(chance / 4) then
+					pl:SetNWBool("Fray Bleeding", false)
+				end
+			end
 		end
 	end
 end)
 
-local chance = Fray.Config.BleedingChance
 hook.Add("EntityTakeDamage", "Fray Bleeding", function(pl, inf)
 	if not pl:IsPlayer() or not (inf:IsDamageType(DMG_BULLET) or inf:IsExplosionDamage()) then
 		return
