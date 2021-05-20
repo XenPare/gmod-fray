17220dbbd0fb41295f0c8422f867bb06bfd79e8e
crester <druggygangsta@gmail.com>
2021-05-21 01:48:25 +0700

    player damage scaling correction

--- gamemode/core/bodyarmor/sv_init.lua
+++ gamemode/core/bodyarmor/sv_init.lua
@@ -32,30 +32,34 @@ timer.Create("Fray Body Armor Broadcast", 15, 0, function()
 end)
 
 hook.Add("ScalePlayerDamage", "Fray Body Armor", function(pl, hit, dmg)
-	if hit == HITGROUP_HEAD then
-		dmg:ScaleDamage(1.3)
-	end
-	if (hit == HITGROUP_CHEST or hit == HITGROUP_STOMACH) then
-		dmg:ScaleDamage(pl:GetNWBool("BA #chest") and 0.6 or 1.1) 
-	end
-	if hit == HITGROUP_LEFTARM or hit == HITGROUP_RIGHTARM then
-		local res = 1
-		if pl:GetNWBool("BA #bicep") then
-			res = res - 0.2
+	if IsValid(pl:GetActiveWeapon()) and pl:GetActiveWeapon():GetClass() == "fray_shield" then
+		dmg:ScaleDamage(0.05)
+	else
+		if hit == HITGROUP_HEAD then
+			dmg:ScaleDamage(1.3)
 		end
-		if pl:GetNWBool("BA #forearm") then
-			res = res - 0.2
+		if (hit == HITGROUP_CHEST or hit == HITGROUP_STOMACH) then
+			dmg:ScaleDamage(pl:GetNWBool("BA #chest") and 0.6 or 1.1) 
 		end
-		dmg:ScaleDamage(res)
-	end
-	if hit == HITGROUP_LEFTLEG or hit == HITGROUP_RIGHTLEG then
-		local res = 1
-		if pl:GetNWBool("BA #calf") then
-			res = res - 0.2
+		if hit == HITGROUP_LEFTARM or hit == HITGROUP_RIGHTARM then
+			local res = 1
+			if pl:GetNWBool("BA #bicep") then
+				res = res - 0.2
+			end
+			if pl:GetNWBool("BA #forearm") then
+				res = res - 0.2
+			end
+			dmg:ScaleDamage(res)
 		end
-		if pl:GetNWBool("BA #thigh") then
-			res = res - 0.2
+		if hit == HITGROUP_LEFTLEG or hit == HITGROUP_RIGHTLEG then
+			local res = 1
+			if pl:GetNWBool("BA #calf") then
+				res = res - 0.2
+			end
+			if pl:GetNWBool("BA #thigh") then
+				res = res - 0.2
+			end
+			dmg:ScaleDamage(res)
 		end
-		dmg:ScaleDamage(res)
 	end
 end)
