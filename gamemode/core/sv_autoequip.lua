604ce00cf4b4ea64930eec090aa7b69f1a4963bd
crester <druggygangsta@gmail.com>
2021-05-21 22:49:02 +0700

    fixed autoequip

--- gamemode/core/sv_autoequip.lua
+++ gamemode/core/sv_autoequip.lua
@@ -1,6 +1,6 @@
 hook.Add("PostPlayerSpawn", "Fray Autoequip", function(pl)
 	for _, class in pairs(pl.Inventory) do
-		if Fray.InventoryList[class].category == FRAY_CATEGORY_WEAPONS then
+		if Fray.InventoryList[class].category == FRAY_CATEGORY_WEAPONS and Fray.InventoryList[class].EquipFunc then
 			Fray.InventoryList[class].EquipFunc(pl)
 		end
 	end
